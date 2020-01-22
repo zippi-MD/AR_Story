04f2d66c7ac05f9d7099afcbf14f1e3a819c3972
Alejandro Mendoza <zippi180@gmail.com>
2020-01-22 12:32:45 -0600

    Initial Commit

---
+++ AR_Story/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AR_Story
+//
+//  Created by Alejandro Mendoza on 22/01/20.
+//  Copyright © 2020 Alejandro Mendoza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
