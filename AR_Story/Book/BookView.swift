ac174ac3e9283c6485fd27f96ee8bd4f90282a16
Alejandro Mendoza <zippi180@gmail.com>
2020-03-02 11:03:25 -0600

    Added base book view

---
+++ AR_Story/Book/BookView.swift
@@ -0,0 +1,16 @@
+//
+//  BookView.swift
+//  AR_Story
+//
+//  Created by Alejandro Mendoza on 02/03/20.
+//  Copyright © 2020 Alejandro Mendoza. All rights reserved.
+//
+
+import UIKit
+
+class BookView: UIView {
+
+    @IBOutlet var containerView: BookView!
+
+
+}
