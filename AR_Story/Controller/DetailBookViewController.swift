4aea63f7dd1f9c7c99239fcabfeda8d76bebf9f2
Alejandro Mendoza <zippi180@gmail.com>
2020-03-03 15:47:48 -0600

    Implemented DetailViewController with contraints for iPhone and iPad

---
+++ AR_Story/Controller/DetailBookViewController.swift
@@ -0,0 +1,27 @@
+//
+//  DetailBookViewController.swift
+//  AR_Story
+//
+//  Created by Alejandro Mendoza on 03/03/20.
+//  Copyright © 2020 Alejandro Mendoza. All rights reserved.
+//
+
+import UIKit
+
+class DetailBookViewController: UIViewController {
+    @IBOutlet weak var coverImageView: UIImageView!
+    @IBOutlet weak var bookTitleLabel: UILabel!
+    @IBOutlet weak var bookDescriptionTextView: UITextView!
+    @IBOutlet weak var startReadingButtonBackgroundView: UIView! {
+        didSet {
+            startReadingButtonBackgroundView.layer.cornerRadius = Constants.cornerRadius
+        }
+    }
+    
+    var book: Book?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
