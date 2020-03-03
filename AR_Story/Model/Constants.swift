a86a1cc4dc972a41756367559ade3a7dfa2bc92b
Alejandro Mendoza <zippi180@gmail.com>
2020-03-03 10:49:32 -0600

    Implemented Constants struct to manage constants in the app

---
+++ AR_Story/Model/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  AR_Story
+//
+//  Created by Alejandro Mendoza on 03/03/20.
+//  Copyright © 2020 Alejandro Mendoza. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let booksFileName = "books"
+    static let booksExtention = "txt"
+}
