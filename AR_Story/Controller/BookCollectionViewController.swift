54b4f2cbbc278309e31db6bba6fc3eff0c3246d4
Alejandro Mendoza <zippi180@gmail.com>
2020-03-03 12:03:24 -0600

    Added BookCollectionViewController

--- AR_Story/Controller/BookCollectionViewController.swift
+++ AR_Story/Controller/BookCollectionViewController.swift
@@ -9,22 +9,21 @@
 import UIKit
 
 class BookCollectionViewController: UIViewController {
-
+    
+    var bookManager: BookManager = BookManager.sharedInstance
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        bookManager.delegate = self
+        bookManager.loadBookCollection()
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+}
+
+extension BookCollectionViewController: BookManagerDelegate {
+    func didFinishLoadingBooks(success: Bool) {
+        
     }
-    */
-
 }
