12defcc5fb82facbeef8fe82b44596b22d6d11c3
eduardoquintero117 <eduardoquiju@hotmail.com>
2020-03-18 23:40:03 -0600

    add functions for speech recognition
    In the extension
    Add authorizationState
    add startRecording

--- AR_Story/ViewController.swift
+++ AR_Story/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController , ARSCNViewDelegate {
     let speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "es-ES"))!
     var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
     var recognitionTask: SFSpeechRecognitionTask?
-    var audioEngine = AVAudioEngine()
+    private var audioEngine = AVAudioEngine()
     
     
     
@@ -28,6 +28,7 @@ class ViewController: UIViewController , ARSCNViewDelegate {
     var foxNode =  FoxCharacter()
     var distance: Float = 0.01
     
+    // MARK: ARImageTracking
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -155,4 +156,94 @@ class ViewController: UIViewController , ARSCNViewDelegate {
     
 }
 
+// MARK: SpeechRecognizer
 
+extension ViewController : SFSpeechRecognizerDelegate {
+    
+    func authorizationState(){
+         SFSpeechRecognizer.requestAuthorization { authStatus in
+                    
+             OperationQueue.main.addOperation {
+                    switch authStatus {
+                        case .authorized:
+                             self.TextViewOfStory.text = "User authorized access to speech recognition"
+                        case .denied:
+                             self.TextViewOfStory.text = "User denied access to speech recognition"
+                        case .restricted:
+                             self.TextViewOfStory.text = "Speech recognition restricted on this device"
+                        case .notDetermined:
+                             self.TextViewOfStory.text = "Speech recognition not yet authorized"
+                                
+                        default:
+                             self.TextViewOfStory.text = "Error AuthStatus"
+                    }
+             }
+        }
+        
+    }
+    
+    
+    
+    private func startRecording() throws {
+        
+        // Cancel the previous task if it's running.
+        recognitionTask?.cancel()
+        self.recognitionTask = nil
+        
+        // Configure the audio session for the app.
+        let audioSession = AVAudioSession.sharedInstance()
+        
+        //.Record
+        //.playAndRecord
+        try audioSession.setCategory(.playAndRecord, mode: .measurement, options: .duckOthers)
+        try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
+        let inputNode = audioEngine.inputNode
+
+        // Create and configure the speech recognition request.
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        guard let recognitionRequest = recognitionRequest else { fatalError("Unable to create a SFSpeechAudioBufferRecognitionRequest object") }
+        recognitionRequest.shouldReportPartialResults = true
+        
+        // Keep speech recognition data on device
+        if #available(iOS 13, *) {
+            //recognitionRequest.requiresOnDeviceRecognition = true
+        }
+        
+        // Create a recognition task for the speech recognition session.
+        // Keep a reference to the task so that it can be canceled.
+        recognitionTask = speechRecognizer.recognitionTask(with: recognitionRequest) { result, error in
+            var isFinal = false
+            
+            if let result = result {
+                // Update the text view with the results.
+                
+                self.TextViewOfStory.text = result.bestTranscription.formattedString
+                isFinal = result.isFinal
+                print("Text-- \(result.bestTranscription.formattedString)")
+            }
+            
+            if error != nil || isFinal {
+                // Stop recognizing speech if there is a problem.
+                self.audioEngine.stop()
+                inputNode.removeTap(onBus: 0)
+
+                self.recognitionRequest = nil
+                self.recognitionTask = nil
+
+                
+            }
+        }
+
+        // Configure the microphone input.
+        let recordingFormat = inputNode.outputFormat(forBus: 0)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer: AVAudioPCMBuffer, when: AVAudioTime) in
+            self.recognitionRequest?.append(buffer)
+        }
+        
+        audioEngine.prepare()
+        try audioEngine.start()
+        
+    }
+    
+    
+}
